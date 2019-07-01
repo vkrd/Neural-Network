e71a97fb3e8f95c8aa846373ccd0f9aab3bc6578
vkrd <49703203+vkrd@users.noreply.github.com>
2019-07-01 01:09:21 -0500

    Added backprop

--- main.py
+++ main.py
@@ -1,35 +1,91 @@
 import numpy as np
+from tqdm import tqdm
 from activ_funcs import *
 from cost_funcs import *
 
-feature_set = np.array([[0,1,0],[0,0,1],[1,0,0],[1,1,0],[1,1,1]])  
-labels = np.array([[1,0,0,1,1]])  
-labels = labels.reshape(5,1)
 
-np.random.seed(69)
-weights = np.random.rand(3,1)
-bias = np.random.rand(1)
+layerType = np.array(["i", "d", "o"])
 
-learning_rate = 0.03
+feature_set = np.array([[0.05, 0.1]])
+labels = np.array([[0.01, 0.99]])
+labels = labels.reshape(1, labels.shape[1])
 
-for e in range(10000):
+np.random.seed(43)
+learning_rate = 0.5
+
+# define network architecture
+arc = np.array([2, 2, 2])
+
+# randomize starting weights and biases
+weights = []
+
+for nodes in range(1,arc.size):
+    temp_arr = np.random.rand(arc[nodes-1],arc[nodes])
+    weights.append(temp_arr)
+
+bias = np.random.rand(arc.size-1,1)
+
+
+def predict(arr):
+    inputs = arr
+    valMat = []
+    inpMat = []
+    for i in range(len(weights)):
+        # calculate dot prod and apply activation func
+        inpMat.append(np.dot(inputs, weights[i]) + bias[i])
+        inputs = sigmoid(np.dot(inputs, weights[i]) + bias[i])
+        valMat.append(inputs.copy())
+
+for e in tqdm(range(10)):
     inputs = feature_set
 
-    #calculate dot product
-    output_vec = np.dot(inputs, weights)+bias
+    valMat = [inputs]
 
-    #apply activation function
-    output_vec = sigmoid(output_vec)
+    # foreward prop
+    for i in range(len(weights)):
+        # calculate dot prod and apply activation func
+        inputs = sigmoid(np.dot(inputs, weights[i]) + bias[i])
+        # print(inputs)
+        valMat.append(inputs.copy())
 
-    #calculate gradient
-    error = output_vec - labels
-    dpred_dcost = d_sigmoid(output_vec)
-    slope = np.dot(inputs.T, error*dpred_dcost) 
+    # output layer
+    output_vec = inputs
+    target = labels
 
-    #update weights and bias
-    weights -= learning_rate * slope
+    # backwards prop
+    nodeDeltaMatrix = [np.empty_like(i) for i in weights]
+    newWeights = [np.copy(i) for i in weights]
 
-    for num in error*dpred_dcost:
-        bias -= learning_rate * num
+    for i in reversed(range(len(weights))):
+        if layerType[i + 1] == "o":
 
-    print(MSE(output_vec,labels))
+            dE_dO = d_MSE(output_vec, target)
+            dO_dnet = d_sigmoid(valMat[i+1])
+            node_delta = dE_dO * dO_dnet
+            nodeDeltaMatrix.insert(i,node_delta)
+
+            dnet_dw = valMat[i]
+
+            slope = np.matmul(dnet_dw.T, node_delta)
+
+            newWeights[i] -= (learning_rate * slope)
+
+
+            bias[i] -= learning_rate * np.mean(slope)
+
+        elif layerType[i+1] == "d":
+            dE_dO = np.matmul(weights[i+1],nodeDeltaMatrix[i+1][:].T).T
+
+            dO_dnet = d_sigmoid(valMat[i+1])
+            node_delta = dE_dO * dO_dnet
+
+            dnet_dw = valMat[i]
+
+            slope = np.matmul(dnet_dw.T, node_delta)
+
+            newWeights[i] = weights[i] - (learning_rate * slope)
+
+            bias[i] -= learning_rate * np.mean(slope)
+
+    #update weights for next pass
+    weights = newWeights
