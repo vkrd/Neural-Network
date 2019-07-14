1bbeeca9b5716fd9b7387ef245b72ee8bfc1f355
vkrd <49703203+vkrd@users.noreply.github.com>
2019-07-14 09:30:04 -0500

    Added new functions

--- activ_funcs.py
+++ activ_funcs.py
@@ -1,7 +1,29 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    # print((1/(1+np.exp(-x))).shape)
+    return 1 / (1 + np.exp(-x))
 
 def d_sigmoid(x):
-    return x*(1-x)
+    # print((x*(1-x)).shape)
+    return x * (1 - x)
+
+def tanh(x):
+    a = np.exp(x)
+    b = np.exp(-x)
+    return np.divide(a - b, a + b)
+
+def d_tanh(x):
+    return 1 - np.square(tanh(x))
+
+def ReLU(x):
+    return np.maximum(x, np.zeros(x.shape))
+
+def d_ReLU(x):
+    return np.where(x <= 0, 0, 1)
+
+def leaky_ReLU(x):
+    return np.where(x > 0, x, x*0.01)
+
+def d_leaky_ReLU(x):
+    return np.where(x > 0, 1, 0.01)
