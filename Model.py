97e66101e799f62490a48fef9a1bb048e0b50c13
vkrd <49703203+vkrd@users.noreply.github.com>
2019-07-14 09:45:42 -0500

    Update and rename main.py to Model.py

--- Model.py
+++ Model.py
@@ -1,13 +1,11 @@
 import numpy as np
 from tqdm import tqdm
-from activ_funcs import *
 from cost_funcs import *
 from LearningRate import LRScheduler
 from layers import *
 
-# test with XOR gate
-feature_set = np.array([[0, 0],[1, 1], [1,0], [0,1]])
-labels = np.array([[0],[0],[1],[1]])
+# print out numbers nicely
+np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format})
 
 class Model:
     def __init__(self):
@@ -19,25 +17,23 @@ class Model:
     def train(self, feature_set, labels, epochs=1000, **kwargs):
         # randomize starting weights and biases
         self.weights = []
-        
+
         # raise errors if not expected input shape
-        if (feature_set.shape[-1] != self.layers[0].nodes):
-            raise ValueError('Mismatching input dimentions (expected ' + str(self.layers[0].nodes) + ' but recieved ' +  str(feature_set.shape[-1]) + ')')
-        elif (labels.shape[-1] != self.layers[-1].nodes):
-            raise ValueError('Mismatching output dimentions (expected ' + str(self.layers[-1].nodes) + ' but recieved ' +  str(labels.shape[-1]) + ')')
-        #np.random.seed(5389)
-        print(feature_set.shape[-1] )
-        
-        for i in range(1,self.layers.size):
-            temp_arr = np.random.rand(self.layers[i-1].nodes,self.layers[i].nodes)
+        if feature_set.shape[-1] != self.layers[0].nodes:
+            raise ValueError(
+                'Mismatching input dimensions (expected ' + str(self.layers[0].nodes) + ' but received ' + str(
+                    feature_set.shape[-1]) + ')')
+        elif labels.shape[-1] != self.layers[-1].nodes:
+            raise ValueError(
+                'Mismatching output dimensions (expected ' + str(self.layers[-1].nodes) + ' but received ' + str(
+                    labels.shape[-1]) + ')')
+        print(feature_set.shape[-1])
+
+        for i in range(1, self.layers.size):
+            temp_arr = np.random.rand(self.layers[i - 1].nodes, self.layers[i].nodes)
             self.weights.append(temp_arr)
 
-        self.bias = np.random.rand(self.layers.size-1,1)
-        '''
-        self.weights.append([[0.15,0.25],[0.20,0.30]])
-        self.weights.append([[0.40,0.50],[0.45,0.55]])
-        self.bias = [0.35,0.60]
-        '''
+        self.bias = np.random.rand(self.layers.size - 1, 1)
 
         # initializing learning rate scheduler
         LRS = LRScheduler()
@@ -62,47 +58,41 @@ class Model:
             output_vec = inputs
             target = labels
 
-            
-            
             # backwards prop
             nodeDeltaMatrix = [np.empty_like(i) for i in self.weights]
             newWeights = [np.copy(i) for i in self.weights]
 
             learning_rate = LRS.nextLR()
-    
+
             for i in reversed(range(len(self.weights))):
                 if self.layers[i + 1].name == "o":
 
                     dE_dO = d_MSE(output_vec, target)
-                    dO_dnet = self.layers[i].d_activation(valMat[i+1])
-                    
-                    node_delta = dE_dO * dO_dnet
-                    
-                    
-                    nodeDeltaMatrix.insert(i,node_delta)
-                    #print(node_delta)
-                    
-                    dnet_dw = valMat[i]
-                    #print(dnet_dw)
-                    slope = np.matmul(dnet_dw.T, node_delta)
+                    dO_dnet = self.layers[i].d_activation(valMat[i + 1])
 
+                    node_delta = dE_dO * dO_dnet
+
+                    nodeDeltaMatrix.insert(i, node_delta)
+
+                    dnet_dw = valMat[i]
+                    
+                    slope = np.matmul(dnet_dw.T, node_delta)
 
                     # update weights and bias
                     newWeights[i] -= (learning_rate * slope)
                     self.bias[i] -= learning_rate * np.mean(slope)
 
-                elif self.layers[i+1].name == "d":
-                    dE_dO = np.matmul(self.weights[i+1],nodeDeltaMatrix[i+1][:].T).T
+                elif self.layers[i + 1].name == "d":
+                    dE_dO = np.matmul(self.weights[i + 1], nodeDeltaMatrix[i + 1][:].T).T
 
-                    dO_dnet = self.layers[i].d_activation(valMat[i+1])
+                    dO_dnet = self.layers[i].d_activation(valMat[i + 1])
                     node_delta = dE_dO * dO_dnet
 
-                    nodeDeltaMatrix.insert(i,node_delta)
-                    
+                    nodeDeltaMatrix.insert(i, node_delta)
+
                     dnet_dw = valMat[i]
 
                     slope = np.matmul(dnet_dw.T, node_delta)
-                    
 
                     # update weights and bias
                     newWeights[i] -= (learning_rate * slope)
@@ -111,20 +101,9 @@ class Model:
             # update weights for next pass
             self.weights = newWeights
 
-    def predict(self,arr):
+    def predict(self, arr):
         inputs = arr
         for i in range(len(self.weights)):
             # calculate dot prod and apply activation func
             inputs = self.layers[i].activation(np.dot(inputs, self.weights[i]) + self.bias[i])
         return inputs
-
-
-# define network architecture
-model = Model()
-
-model.add_layer(Dense(2, name="i", activation="tanh"))
-model.add_layer(Dense(4, activation="tanh"))
-model.add_layer(Dense(1, name="o"))
-
-model.train(feature_set, labels, epochs=10000, learning_rate=0.1)
-print(model.predict(np.array([[0, 0],[1, 1], [1,0], [0,1]])))
