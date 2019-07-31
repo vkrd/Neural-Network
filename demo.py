814d716418e3c9093ff855cfcb43a29e4e1f973c
vkrd <49703203+vkrd@users.noreply.github.com>
2019-07-31 10:29:33 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,6 +11,6 @@ model.add_layer(Dense(2, name="i", activation="tanh"))
 model.add_layer(Dense(5, activation="tanh"))
 model.add_layer(Dense(1, name="o"))
 
-model.train(feature_set, labels, epochs=10000, learning_rate=0.1)
+model.train(feature_set, labels, epochs=10000, learning_rate=0.1, momentum=0.9)
 
 print(model.predict(np.array([[0, 0], [1, 1], [1, 0], [0, 1]])))
