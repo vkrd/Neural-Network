220b71dc5289b4ef7b2bb811dc2d6dd8f72d8400
vkrd <49703203+vkrd@users.noreply.github.com>
2019-08-01 15:34:51 -0500

    Comments are nice

--- demo.py
+++ demo.py
@@ -11,10 +11,15 @@ model.add_layer(Dense(2, name="i", activation="sig"))
 model.add_layer(Dense(3, activation="sig"))
 model.add_layer(Dense(1, name="o"))
 
+# start training the model
 model.train(feature_set, labels, epochs=10000, learning_rate=0.1, momentum=0.8)
 
+# see how well training worked
 print(model.predict(np.array([[0, 0], [1, 1], [1, 0], [0, 1]])))
 
+# save model
 model.save_model("model")
+
+# load model
 model.load_architecture("model.arc")
 model.load_weights("model.weights")
