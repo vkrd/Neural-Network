469c0c8ff519bae92945adbe947b7efc3cbccd71
vkrd <49703203+vkrd@users.noreply.github.com>
2019-08-01 15:33:58 -0500

    Save/Load demo

--- demo.py
+++ demo.py
@@ -7,10 +7,14 @@ labels = np.array([[0], [0], [1], [1]])
 # define network architecture
 model = Model()
 
-model.add_layer(Dense(2, name="i", activation="tanh"))
-model.add_layer(Dense(5, activation="tanh"))
+model.add_layer(Dense(2, name="i", activation="sig"))
+model.add_layer(Dense(3, activation="sig"))
 model.add_layer(Dense(1, name="o"))
 
-model.train(feature_set, labels, epochs=10000, learning_rate=0.1, momentum=0.9)
+model.train(feature_set, labels, epochs=10000, learning_rate=0.1, momentum=0.8)
 
 print(model.predict(np.array([[0, 0], [1, 1], [1, 0], [0, 1]])))
+
+model.save_model("model")
+model.load_architecture("model.arc")
+model.load_weights("model.weights")
